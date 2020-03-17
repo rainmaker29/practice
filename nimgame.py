edf00c48ef13fb48cf169aad8482dde637560afb
rainmaker29 <amaanrahil29@gmail.com>
2020-03-17 17:52:31 +0530

    Nim game update

--- nimgame.py
+++ nimgame.py
@@ -15,3 +15,6 @@ else:
 # Find the cumulative XOR
 # If the XOR comes out 0 then the second player wins
 # Otherwise the first one wins
+
+# Also, Nimgame isn't affected if a concept of adding stones/blocks to piles is introduced
+# Because, if a player adds X number of stones, the other player nullifies this move by removing those stones
